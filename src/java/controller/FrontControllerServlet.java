aff1d8c8e364f9ada942b3987bf152846afca16f
tendrymbolanekena <tendrymbolanekena@gmail.com>
2026-06-11 22:59:09 +0300

    creation de sprint 0 -- FrontControllerServlet

---
+++ src/java/controller/FrontControllerServlet.java
@@ -0,0 +1,27 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class FrontControllerServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req,HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getRequestURI();
+
+        resp.setContentType("text/html");
+
+        resp.getWriter().write("<html><body><h1>Front Controller Servlet</h1><p>lien: " + path + "</p></body></html>");
+    }
+
+    protected void doPost(HttpServletRequest req,HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getRequestURI();
+
+        resp.setContentType("text/html");
+
+        resp.getWriter().write("<html><body><h1>Front Controller Servlet</h1><p>lien: " + path + "</p></body></html>");
+    }
+
+}
